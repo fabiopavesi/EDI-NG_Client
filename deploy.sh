f188de3214254a2a2ed3921d1a48b2cad99c2602
Fabio Pavesi <fabio@adamassoft.it>
2018-12-16 18:13:49 +0100

    Added deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# ng build --prod --build-optimizer --base-href '/edi/' && \
+# scp -C -r dist/* root@enygma.it:/var/www/sdi/edi/
+
+ng build --prod --build-optimizer
+docker build -t edi-client .
+docker stop edi-client
+docker rm edi-client
+docker run --name edi-client -v $(pwd)/rubbish/:/usr/share/nginx/html/assets/templates/ -d -p 80:80 edi-client
